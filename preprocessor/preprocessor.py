3ae553252a2cc1611a2c8289c0d399a0bd83068c
myroncaldwell <mmyroncaldwell@gmail.com>
2021-05-11 22:34:17 +0300

    to_grayscale method

---
+++ preprocessor/preprocessor.py
@@ -0,0 +1,16 @@
+import cv2 as cv
+
+
+def to_grayscale(image):
+    """ Converts an RGB image to grayscale.
+
+    Args:
+        image:
+            RGB image, as numpy array.
+
+    Returns:
+        Grayscale image, as numpy array.
+    """
+    grayscale = cv.cvtColor(image, cv.COLOR_RGB2GRAY)
+
+    return grayscale
