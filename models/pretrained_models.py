05101db6de75f364e5e942afe9cbf80f84b3e614
myroncaldwell <mmyroncaldwell@gmail.com>
2021-10-23 22:12:34 +0300

    added functions to load and config pretrained models (alexnet, vgg-16, googlenet, resnet-50)

---
+++ models/pretrained_models.py
@@ -0,0 +1,80 @@
+from torch import nn
+import torchvision.models as models
+
+
+def set_parameter_requires_grad(model, feature_extracting):
+    """ This helper function sets the .requires_grad attribute of the parameters in the model to False when we
+    are feature extracting.
+
+    When we are feature extracting and only want to compute gradients for the newly initialized layer, then we
+    want all of the other parameters to not require gradients.
+
+    Args:
+        model:
+            deep learning model, as pytorch object.
+        feature_extracting:
+            whether or not we're feature extracting, as boolean.
+    """
+    if feature_extracting:
+        for param in model.parameters():
+            param.requires_grad = False
+
+
+def initialize_model(model_name, pretrained, num_labels, feature_extracting):
+    """ Initialize pretrained deep-learning model and reshape the last layer with the correct number of classes.
+
+    The supported pretrained models are:
+        * AlexNet
+        * VGG-16
+        * GoogLeNet
+        * ResNet-50
+    Since these deep-learning models have been pretrained on Imagenet, they have output layers of size 1000,
+    one node for each class. We reshape the last layers to have the same number of inputs as before, and to
+    have the same number of outputs as the number of classes in our the dataset.
+
+    Args:
+        model_name:
+            model name (alexnet, vgg-16, googlenet, resnet-50), as string.
+        pretrained:
+            whether or not we want the pretrained version, as boolean.
+        num_labels:
+            number of labels in our dataset, as integer.
+        feature_extracting:
+          flag for feature extracting (when False, we finetune the whole model, when True we only update the
+          reshaped layer params), as boolean.
+
+    Returns:
+        deep-learning model, as pytorch object.
+    """
+    model = None
+
+    if model_name == 'alexnet':
+        model = models.alexnet(pretrained=pretrained)
+
+        set_parameter_requires_grad(model, feature_extracting)
+
+        last_layer_in_ftrs = model.classifier[6].in_features
+        model.classifier[6] = nn.Linear(last_layer_in_ftrs, num_labels)
+    elif model_name == 'vgg-16':
+        model = models.vgg16(pretrained=pretrained)
+
+        set_parameter_requires_grad(model, feature_extracting)
+
+        last_layer_in_ftrs = model.classifier[6].in_features
+        model.classifier[6] = nn.Linear(last_layer_in_ftrs, num_labels)
+    elif model_name == 'googlenet':
+        model = models.googlenet(pretrained=pretrained)
+
+        set_parameter_requires_grad(model, feature_extracting)
+
+        last_layer_in_ftrs = model.fc.in_features
+        model.fc = nn.Linear(last_layer_in_ftrs, num_labels)
+    elif model_name == 'resnet-50':
+        model = models.resnet50(pretrained=pretrained)
+
+        set_parameter_requires_grad(model, feature_extracting)
+
+        last_layer_in_ftrs = model.fc.in_features
+        model.fc = nn.Linear(last_layer_in_ftrs, num_labels)
+
+    return model
